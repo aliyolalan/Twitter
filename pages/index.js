a86406aaf77fae5c799d91c101fe267c2b31bc22
aliyolalan <yolalanali1@gmail.com>
2020-08-12 02:34:17 +0300

    Proje dosyları oluşturuldu.

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Button from '../components/Button'
+
+function HomePage() {
+    return <div>
+      <h3>Here Comes The Rain Again</h3>
+      <Button>Merhaba</Button>
+    </div>
+
+    
+  }
+  
+  export default HomePage
